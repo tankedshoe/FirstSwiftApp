e94ab088e1e60ffbe380298797ecc914af480890
tankedshoe <tankedshoe@gmail.com>
2017-10-06 09:22:08 -0600

    Added a background color randomiser.

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -28,6 +28,18 @@ class ViewController: UIViewController
         {
             firstButton.backgroundColor = .orange
         }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
     }
     
     override func didReceiveMemoryWarning()
